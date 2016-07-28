5869c91f28d397ef33b86747e22443fbf34a3018
MiningMarsh <miningmarsh@gmail.com>
2016-07-27 22:54:41 -0400

    Made crons run in parallel.

--- code/environments/production/modules/cron/manifests/config.pp
+++ code/environments/production/modules/cron/manifests/config.pp
@@ -6,6 +6,12 @@ class cron::config {
 
   $persistent_directory = persistent::directory('cron')
 
+  $run_and_remove_lock_name = 'puppet-cron-run-and-remove-lock'
+  script::install { $run_and_remove_lock_name:
+    content => file('cron/run-and-remove-lock'),
+  }
+  $run_and_remove_lock = script::path($run_and_remove_lock_name)
+
   script::install { 'puppet-run-crons':
     content => template('cron/run-crons.erb'),
   }

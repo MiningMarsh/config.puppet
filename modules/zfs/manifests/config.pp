487c5459686d0668cca8f2ba889d9ab05c3e0146
MiningMarsh <miningmarsh@gmail.com>
2019-09-08 16:58:09 -0400

    Added script that cleans old snapshots on zfs systems.

--- modules/zfs/manifests/config.pp
+++ modules/zfs/manifests/config.pp
@@ -5,8 +5,12 @@ class zfs::config {
   $zfs_scrub_name = 'zfs-scrub'
   $zfs_scrub = script::path($zfs_scrub_name)
 
+  $remove_bad_snapshots_name = 'remove-bad-snapshots'
+  $remove_bad_snapshots = script::path($remove_bad_snapshots_name)
+
   $scripts = [
     $zfs_scrub_name,
+    $remove_bad_snapshots_name
   ]
 
   $scripts.each |String $script| {
@@ -17,6 +21,11 @@ class zfs::config {
 
   batch::entry { 'zfs-scrub':
     command => $zfs_scrub,
-    period => monthly,
+    period => monthly
+  }
+
+  batch::entry { 'remove-bad-snapshots':
+    command => $remove_bad_snapshots,
+    period => hourly
   }
 }

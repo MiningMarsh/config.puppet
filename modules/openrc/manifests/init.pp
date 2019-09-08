18669e8895a785996651b155cec0c34194d665f5
MiningMarsh <miningmarsh@gmail.com>
2019-09-08 12:11:46 -0400

    Ensure monit is installed before we attempt to use it.

--- modules/openrc/manifests/init.pp
+++ modules/openrc/manifests/init.pp
@@ -2,11 +2,16 @@ class openrc {
   require run
 
   $prefix = '/etc/monit.d' 
+  $lib = '/var/lib/monit' 
   $config = '/etc/monitrc'
 
   package { monit:
     ensure => installed
   }
+
+  file { $lib:
+    ensure => directory
+  } ->
 
   file { $prefix:
     ensure => directory

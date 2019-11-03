9d1a030fa5ea3c90c2e6c8b8932aed44381fa9d0
MiningMarsh <miningmarsh@gmail.com>
2019-11-03 09:03:02 -0500

    Changed puppet.cron such that modules are installed before the configuration is deployed.

--- modules/os/manifests/all.pp
+++ modules/os/manifests/all.pp
@@ -3,7 +3,7 @@ class os::all {
   # Automatically update puppet on every machine.
   # TODO: Factor this out into a template because frankly this is getting ridiculous.
   run::entry { 'puppet':
-    command => 'cd /etc/puppet; git pull; puppet apply --modulepath=/etc/puppet/modules /etc/puppet/site.pp; puppet module install puppetlabs-stdlib --target-dir /etc/puppet/modules; jq .dependencies -c < /etc/puppet/manifest.json | grep \'"[^"]*"\' -o | cut -f2 -d\'"\' | xargs -n1 puppet module install --target-dir /etc/puppet/modules; if [[ -d /etc/puppetlabs ]]; then rm -rf /etc/puppetlabs; fi',
+    command => 'cd /etc/puppet; git pull; puppet module install puppetlabs-stdlib --target-dir /etc/puppet/modules; jq .dependencies -c < /etc/puppet/manifest.json | grep \'"[^"]*"\' -o | cut -f2 -d\'"\' | xargs -n1 puppet module install --target-dir /etc/puppet/modules; puppet apply --modulepath=/etc/puppet/modules /etc/puppet/site.pp; if [[ -d /etc/puppetlabs ]]; then rm -rf /etc/puppetlabs; fi',
     period  => daily
   }
 
